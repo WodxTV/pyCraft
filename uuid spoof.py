2e7790f23796b7887af5b065c6f34167b68b3f7a
wodx <38795504+WodxTV@users.noreply.github.com>
2019-12-25 18:56:54 +0100

    Update uuid spoof.py

--- uuid spoof.py
+++ uuid spoof.py
@@ -2,12 +2,19 @@ from minecraft.networking.connection import Connection
 from minecraft.networking.packets import clientbound, serverbound
 
 def join_game_event(packet):
-    print('[*] Connected')
+    print('[+] connected')
     chat_packet = serverbound.play.ChatPacket()
     chat_packet.message = 'I am not a bot'
     bot.write_packet(chat_packet)
 
 if __name__ == '__main__':
-    bot = Connection(address='127.0.0.1', port=25565, username='pyCraft', spoof_uuid='069a79f444e94726a5befca90e38aaf5', spoof_ip='255.255.255.255')
+    bot = Connection(
+        address='127.0.0.1',
+        port=25565,
+        username='pyCraft',
+        spoof_uuid='069a79f444e94726a5befca90e38aaf5', # note: spoof_uuid should not contain dash '-' characters
+        spoof_ip='255.255.255.255'
+    )
     bot.register_packet_listener(join_game_event, clientbound.play.JoinGamePacket)
+    print('[*] connecting...')
     bot.connect()
